aee797d92ecb3f8b422ac87136b037fe1f4aad30
Tina <113181634+chrisVolkova@users.noreply.github.com>
2022-12-23 07:13:55 +0400

    268. Missing Number

---
+++ 268. Missing Number.py
@@ -0,0 +1,18 @@
+"""
+Link: https://leetcode.com/problems/missing-number/description/
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only
+number in the range that is missing from the array.
+"""
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        sort_nums = sorted(nums)
+        i = 0
+        while i + 1 < len(sort_nums):
+            if sort_nums[i] + 1 != sort_nums[i + 1]:
+                return sort_nums[i] + 1
+            i += 1
+        if sort_nums[0] > 0:
+            return sort_nums[0] - 1
+        return sort_nums[i] + 1
